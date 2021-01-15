ab134708fbad01d567a7bd8d4b19ac8c28cfee61
Mollie Goforth <molliegoforth818@gmail.com>
2021-01-15 13:44:10 -0600

    testing map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import logo from './logo.svg';
+import React, {Component} from 'react'
+import L from 'leaflet';
 import './App.css';
-import axios from 'axios';
 import SearchPage from './components/SearchPage.js';
+import {Map, TileLayer, Marker, Popup} from react-leaflet
 
-function App() {
+class App extends Component() {
+  state = {
+    redIcon:{
+    lat = 40.713,
+    lng = -73.9,
+  },
+  zoom = 13,
+  }
+  redIcon = L.icon({
+    iconUrl: leafRed,
+    shadowUrl: leafShadow,
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -86]
+  });
+
+  render(){
+    const positionRedIcon = [this.state.redIcon.lat, this.state.redIcon.lng];
   return (
     <div className="App">
       <SearchPage />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={positionRedIcon} icon={this.redIcon}>
+          <Popup>
+            <span>
+              This is Henry Street Settlement
+            </span>
+          </Popup>
+        </Marker>
+      </Map>
     </div>
   );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import GoodNFull from './components/GoodNFull';
+import 'leaflet/dist/leaflet.css'
 
 
 ReactDOM.render(
